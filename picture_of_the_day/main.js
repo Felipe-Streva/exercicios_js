aa67633fd94e3adc267c4eb1d91e19ecb2ea0d33
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-03 10:15:33 -0300

    tratando erros

--- picture_of_the_day/main.js
+++ picture_of_the_day/main.js
@@ -11,25 +11,41 @@ const myRequest = new XMLHttpRequest();
 myRequest.open("GET", "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
 
 myRequest.addEventListener("load", function(){
-    let aux = JSON.parse((myRequest.responseText))
 
-    let body = document.querySelector('body')
+    if(myRequest.status==200){
+        let aux = JSON.parse((myRequest.responseText))
 
-    const h2 = elementFactory('h2','title', 'title', aux.title);
-    body.appendChild(h2)
+        let body = document.querySelector('body')
 
-    const iframe = elementFactory('iframe','iframe', 'iframe', '');
-    iframe.src = aux.url
-    body.appendChild(iframe)
+        const h2 = elementFactory('h2','title', 'title', aux.title);
+        body.appendChild(h2)
 
-    const data = elementFactory('p','date', 'date', aux.date);
-    body.appendChild(data)
+        if(aux.media_type == 'image'){
+            const img = elementFactory('img','foto','foto','')
+            if(aux.hdurl != undefined ){
+                img.src = aux.hdurl
+                body.appendChild(img)
+            } else {
+                img.src = aux.url
+                body.appendChild(img)
+            }
+        } else {
+            const iframe = elementFactory('iframe','iframe', 'iframe', '');
+            iframe.src = aux.url
+            body.appendChild(iframe)
+        }
 
-    const text = elementFactory('p','text', 'date', aux.explanation);
-    body.appendChild(text)
+        const data = elementFactory('p','date', 'date', aux.date);
+        body.appendChild(data)
 
-    const copy = elementFactory('p','copy', 'date', '');
-    copy.innerHTML = `&copy; ${aux.copyright}` 
-    body.appendChild(copy)
+        const text = elementFactory('p','text', 'date', aux.explanation);
+        body.appendChild(text)
+
+        const copy = elementFactory('p','copy', 'date', '');
+        copy.innerHTML = `&copy; ${aux.copyright}` 
+        body.appendChild(copy)
+    } else {
+        window.location.href = 'erro.html'
+    }
 })
 myRequest.send()
