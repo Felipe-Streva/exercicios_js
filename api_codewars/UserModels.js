42dd6c681cd54e83135632ab18347ba768ae2c79
Felipe Streva <fstreva@poli.ufrj.br>
2020-07-10 10:29:29 -0300

    Inicialize the Codewars Stats

---
+++ api_codewars/UserModels.js
@@ -0,0 +1,8 @@
+class UserModels{
+    constructor(user, honor, kyu, languages){
+        this._user = user;
+        this._honor = honor;
+        this._kyu = kyu;
+        this._languages = languages // é um objeto, usar o obj.keys()
+    }
+}
